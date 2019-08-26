55e7bf5c5e3d409a678995760e7322f8814e92d5
Jeffrey Santana <SantanaJny@gmail.com>
2019-08-26 11:15:31 -0400

    Initial commit

---
+++ Foodie Fun/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Foodie Fun
+//
+//  Created by Jeffrey Santana on 8/26/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
