db1f609ae0e0da2dd92332f74f13a43fc08ec46d
Jeffrey <SantanaJny@gmail.com>
2019-08-29 15:50:54 -0400

    Refactoring account ui and creating classes to manage labels/textviews

--- Foodie Fun/Views/BorderLabel.swift
+++ Foodie Fun/Views/BorderLabel.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2019 Lambda. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+@IBDesignable
+class BorderLabel: UILabel {
+	
+	override func awakeFromNib() {
+		setupView()
+	}
+	
+	override func drawText(in rect: CGRect) {
+		let insets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+		super.drawText(in: rect.inset(by: insets))
+	}
+	
+	override func prepareForInterfaceBuilder() {
+		super.prepareForInterfaceBuilder()
+		setupView()
+	}
+	
+	func setupView() {
+		layer.borderWidth = 1
+		layer.borderColor = UIColor.lightGray.cgColor
+		layer.cornerRadius = 10
+		layer.masksToBounds = true
+		
+		heightAnchor.constraint(equalToConstant: 30).isActive = true
+	}
+	
+}
--- Foodie Fun/Views/BorderTextView.swift
+++ Foodie Fun/Views/BorderTextView.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2019 Lambda. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+@IBDesignable
+class BorderTextView: UITextView {
+	
+	override func awakeFromNib() {
+		setupView()
+	}
+	
+	override func prepareForInterfaceBuilder() {
+		super.prepareForInterfaceBuilder()
+		setupView()
+	}
+	
+	func setupView() {
+		layer.borderWidth = 1
+		layer.borderColor = UIColor.lightGray.cgColor
+		layer.cornerRadius = 10
+		layer.masksToBounds = true
+		
+	}
+	
+}
